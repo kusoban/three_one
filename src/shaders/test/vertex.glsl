b085ec8f4ff08561d442df590a478972f838e38f
george <nakhodko@gmail.com>
2021-07-01 17:40:23 +0300

    jelly waves

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -10,7 +10,8 @@ void main()
 {
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 	modelPosition.z  = sin(modelPosition.x * uFrequency.x + uTime) - .5;
-	modelPosition.y  += sin(modelPosition.y * uFrequency.y + uTime) * 0.05;
+	modelPosition.x  += sin(modelPosition.y * uFrequency.y + uTime) * 0.05;
+	modelPosition.z  += sin(modelPosition.y * uFrequency.y + uTime) * 0.05;
 
 	distance = modelPosition.z;
 	 
